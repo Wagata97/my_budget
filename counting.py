f0560fd2c8dadf0463066907f394ed6ca2df811a
Agata <agata.wycislik97@gmail.com>
2023-05-12 17:55:28 +0200

    Before adding functions

--- counting.py
+++ counting.py
@@ -1,15 +1,34 @@
-print("""
-Hello in App My Budget.
-Please choose option which you are interested:
- 1 - payment 
- 2 - paycheck
- 3 - balance
-  """)
+def main():
+    global user_choose
+    print("""
+        Hello in App My Budget.
+        Please choose option which you are interested:
+        1 - payment 
+        2 - paycheck
+        3 - balance""")
 
-my_balance = 0
-choose = input('> ')
+    my_balance = 0
 
-while True:
-    if not choose.isdecimal():
-        print('Incorrect data. Please, try again.')
-    pass
+    while True:
+        choose = input('> ')
+        if not choose.isdecimal():
+            print('Please, enter a number')
+        else:
+            user_choose = int(choose)
+            break
+
+    if user_choose == 1:
+        x = int(input('your income: '))
+        my_balance += x
+    elif user_choose == 2:
+        x = int(input('your expense: '))
+        my_balance -= x
+    elif user_choose == 3:
+        print('your balance: {}'.format(my_balance))
+    else:
+        print('incorrect data. Try again')
+
+
+if __name__ == "__main__":
+    main()
+
