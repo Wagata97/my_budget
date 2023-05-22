e57b0c5c6971353dc8472e538f8ed1a77ac590a9
Agata <agata.wycislik97@gmail.com>
2023-05-22 19:52:05 +0200

    amendments

--- counting.py
+++ counting.py
@@ -4,11 +4,12 @@ choose = {1: 'PAYMENT', 2: 'PAYCHECK', 3: 'BALANCE'}
 def main():
     while True:
         print("""
-               Hello in App My Budget.
-               Please choose option which you are interested:
-               1 - payment 
-               2 - paycheck
-               3 - balance""")
+    Hello in App My Budget.
+    Please choose option which you are interested:
+      1 - payment 
+      2 - paycheck
+      3 - balance
+    """)
         user_choose = int(input('My choose > '))
         if user_choose == 1:
             income()
@@ -28,17 +29,18 @@ def income():
     global my_balance
     payment = int(input('Your income: '))
     my_balance += payment
-    print(my_balance)
+    print('My balance: ', my_balance)
 
 
 def expense():
     global my_balance
     paycheck = int(input('Your expense: '))
     my_balance += paycheck
+    print('My balance: ', my_balance)
 
 
 def balance():
-    print(my_balance)
+    print('My balance: ', my_balance)
 
 
 if __name__ == "__main__":
